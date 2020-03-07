8ed4048626e7bd62a67e2227e41cf1fa468e5ae9
lizhanxiang <lizhanxiang@meituan.com>
2020-03-07 18:28:54 +0800

    添加键盘监听事件

--- src/com/mashibing/tank/T.java
+++ src/com/mashibing/tank/T.java
@@ -1,9 +1,5 @@
 package com.mashibing.tank;
 
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 public class T {
     public static void main(String[] args) {
         TankFrame tk = new TankFrame();
--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -1,10 +1,13 @@
 package com.mashibing.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
+    int x =200 ,y=200;
     public TankFrame()  {
         //设置宽、高
         this.setSize(800,600);
@@ -13,6 +16,7 @@ public class TankFrame extends Frame {
         this.setTitle("tank war");
         //设置可见
         this.setVisible(true);
+        this.addKeyListener(new MyKeyListener());
         //添加window监听器
         this.addWindowListener(new WindowAdapter() {
 
@@ -24,12 +28,28 @@ public class TankFrame extends Frame {
     }
 
     /**
-     * 窗口打开的时候调用
+     * 每次触发前会先清空画板
      * @param g
      */
     @Override
     public void paint(Graphics g){
-//        System.out.println("paint");
-        g.fillRect(200,200,50,50);
+        System.out.println("paint");
+        g.fillRect(x,y,50,50);
+        y+=10;
+        x+=10;
+    }
+
+    class MyKeyListener extends KeyAdapter{
+
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println("key pressd...");
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("key released...");
+        }
     }
 }
